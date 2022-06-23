5dc691d3b1caf669eeed790741f21e9391fc0229
Divyanshu raj <98219015+its-divyanshu-raj@users.noreply.github.com>
2022-06-23 09:26:37 +0530

    Create #1output.cpp

---
+++ #1output.cpp
@@ -0,0 +1,18 @@
+/* What is the output
+
+What will be the output of the following code ? */ 
+
+#include <iostream>
+using namespace std;
+int main(){
+    cout << code ;
+}
+
+/* Options
+This problem has only one correct answer
+(1)code
+(2)Error 
+Correct Answer 
+
+Solution Description
+If we want to print something on screen exactly, we need to put that text in double quotes, otherwise error will come. For eg. */ cout << "code";
