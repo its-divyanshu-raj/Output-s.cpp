f598fa100c5381bdf648f78f2ac10640c951ce67
Divyanshu raj <98219015+its-divyanshu-raj@users.noreply.github.com>
2022-06-26 00:19:59 +0530

    Create output-5.cpp

---
+++ output-5.cpp
@@ -0,0 +1,18 @@
+/* Which of the following gives the memory address of integer variable a ?
+
+
+
+You have max 2 attempts to score in this question.
+
+Attempts left:
+1/2
+Options
+
+This problem may have one or more correct answers
+
+*a;
+a;
+&a; 
+address(a); */ 
+
+Correct Answer = &a; 
